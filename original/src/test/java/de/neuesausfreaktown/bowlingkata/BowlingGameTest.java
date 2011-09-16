60cedb16c20aa88a0c46102e481eb23e754678e3
Niels at office <niels.coelle@secunet.com>
2011-09-16 09:50:04 +0200

    Kata completed

--- original/src/test/java/de/neuesausfreaktown/bowlingkata/BowlingGameTest.java
+++ original/src/test/java/de/neuesausfreaktown/bowlingkata/BowlingGameTest.java
@@ -11,6 +11,8 @@ package de.neuesausfreaktown.bowlingkata;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -19,14 +21,68 @@ import org.junit.Test;
  * @since 1.5
  */
 public class BowlingGameTest {
-    @Test
-    public void testGutterGame() {
-        Game g = new Game();
 
-        for (int i = 0; i < 20; i++) {
-            g.roll(0);
-        }
+    protected Game g;
+
+    @Before
+    public void setUp() throws Exception {
+        g = new Game();
+    }
+
+    @Test
+    public void allGutters() {
+        final int rolls = 20;
+        final int pins = 0;
+
+        rollMany(rolls, pins);
 
         assertThat(g.score(), is(0));
+    }
+
+    @Test
+    public void allOnes() {
+        final int rolls = 20;
+        final int pins = 1;
+
+        rollMany(rolls, pins);
+        assertThat(g.score(), is(20));
+    }
+
+    @Test
+    public void oneSpareRoundRestGutter() {
+        rollSpare();
+        g.roll(3);
+        rollMany(17, 0);
+        assertThat(g.score(), is(16));
+    }
+
+    @Test
+    public void oneStrike() {
+        rollStrike();
+        g.roll(3);
+        g.roll(4);
+        rollMany(16, 0);
+        assertThat(g.score(), is(24));
+    }
+
+    @Test
+    public void perfectGame() {
+        rollMany(12, 10);
+        assertThat(g.score(), is(300));
+    }
+
+    private void rollStrike() {
+        g.roll(10);
+    }
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
+    }
+
+    private void rollMany(final int rolls, final int pins) {
+        for (int i = 0; i < rolls; i++) {
+            g.roll(pins);
+        }
     }
 }
--- original/src/test/java/de/neuesausfreaktown/bowlingkata/Game.java
+++ original/src/test/java/de/neuesausfreaktown/bowlingkata/Game.java
@@ -17,14 +17,48 @@ import org.slf4j.LoggerFactory;
  * @since 1.5
  */
 public class Game {
-    /** Logger der Klasse. */
-    private static final Logger LOG = LoggerFactory.getLogger(Game.class);
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
 
     public void roll(final int pins) {
-
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
-        return 0;
+        int score = 0;
+        int frameIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex++;
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += sumOfBinsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isStrike(final int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
+
+    private int sumOfBinsInFrame(final int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+    private int spareBonus(final int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int strikeBonus(final int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
+    private boolean isSpare(final int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
     }
 }
